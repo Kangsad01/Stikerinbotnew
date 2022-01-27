eb71711638da3f292ac806455e968ff371d7e1ed
The.sad.boy01 <88497118+Kangsad01@users.noreply.github.com>
2022-01-27 12:29:51 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,13 @@
+pkg update && pkg upgrade
+pkg install git -y
 pkg install nodejs -y
 pkg install ffmpeg -y
 pkg install imagemagick -y
-npm install
+pkg install yarn
+pkh install python
+git clone https://github.com/Kangsad01/Stikerinbotnew
+cd Stikerinbotnew
+yarn
+npm i
 npm update
+node .
